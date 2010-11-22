36bac117fa375ed20d5800c22351748caf9cf80d
Alexander Kuzmin <roosit@abricos.org>
2010-11-22 15:33:14 +0000

    Модуль "Notify": добавление в основную ветку из branches

---
+++ includes/manager.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * @version $Id$
+ * @package Abricos
+ * @subpackage Notify
+ * @copyright Copyright (C) 2008 Abricos. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @author Alexander Kuzmin (roosit@abricos.org)
+ */
+
+class NotifyManager extends Notification {
+	
+	/**
+	 * 
+	 * @var NotifyModule
+	 */
+	public $module = null;
+	
+	public $core = null;
+
+	/**
+	 * 
+	 * @var CMSDatabase
+	 */
+	public $db = null;
+	
+	public $user = null;
+	public $userid = 0;
+	
+	public function NotifyManager(NotifyModule $module){
+		
+		$this->module = $module;
+		$core = $module->registry;
+		$this->core = $core;
+		$this->db = $core->db;
+		
+		$this->user = $core->user->info;
+		$this->userid = $this->user['userid'];
+	}
+	
+	public function SendMail($email, $subject, $message){
+		
+		/*
+		// настройки конфига
+		$config['module']['notify']['type'] = "abricos";
+		
+		/**/
+		
+		switch(CMSRegistry::$instance->config['module']['notify']['type']){
+			case 'abricos':
+				return $this->SendByAbricos($email, $subject, $message);
+			default: 
+				return $this->SendByMailer($email, $subject, $message);
+		}
+	}
+	
+	public function SendByMailer($email, $subject, $message){
+		$mailer = new NotifyMailer();
+		if (!$mailer->ValidateAddress($email)){
+			return false;
+		}
+		$mailer->Subject = $subject;
+		$mailer->MsgHTML($message);
+		$mailer->AddAddress($email);
+		$result = $mailer->Send();
+		return $result;
+	}
+	
+	public function SendByAbricos($email, $subject, $message){
+		$mailer = new NotifyAbricos();
+		$mailer->email = $email;
+		$mailer->subject = $subject;
+		$mailer->message = $message;
+		$mailer->Send();
+	}
+}
+
+class NotifyAbricos {
+	
+	public $fromName = "";
+	public $from = "";
+	public $email = "";
+	public $subject = "";
+	public $message = "";
+	
+	private $host = "";
+	private $password = "";
+	
+	public function NotifyAbricos(){
+		
+		$this->from = Brick::$builder->phrase->Get('sys', 'admin_mail'); 
+		$this->fromName = Brick::$builder->phrase->Get('sys', 'site_name');
+		
+		$cfg = &CMSRegistry::$instance->config['module']['notify'];
+		$this->host = $cfg['host'];
+		$this->password = $cfg['password'];
+	}
+
+	private function EncodeParam($arr){
+		$data = array();
+		foreach($arr as $name => $value){
+			array_push($data, $name."=".urlencode($value));
+		}
+		return implode("&", $data);
+	}
+	public function Send(){
+		$data = $this->EncodeParam(array(
+			"from"=>$this->from,
+			"fromname"=>$this->fromName,
+			"body"=>$this->message,
+			"subject"=>$this->subject,
+			"to"=>$this->email,
+			"password" => $this->password
+		));
+		
+		$fp = fsockopen($this->host, 80, $errno, $errstr, 10);
+		if ($fp) {
+		
+			$out = "POST /mailer/send/ HTTP/1.1\r\n";
+			$out .= "Host: ".$this->host."\r\n";
+			$out .= "User-Agent: Opera/8.50 (Windows NT 5.1; U; ru)\r\n";
+			$out .= "Content-Type: application/x-www-form-urlencoded\r\n";
+			$out .= "Content-Length: ".strlen($data)."\r\n\r\n";
+			$out .= $data."\r\n\r\n";
+		
+			fputs($fp, $out); // отправляем данные
+			
+			/*
+			while($gets=fgets($fp,2048)){
+				// print $gets;
+			}
+			/**/
+			fclose($fp);
+		}
+		return true;
+	}
+}
+
+class NotifyMailer extends PHPMailer {
+	
+	/**
+	 * 
+	 * @var CMSRegistry
+	 */
+	public $core = null;
+	
+	public function NotifyMailer(){
+		$this->core = CMSRegistry::$instance;
+		
+		$this->FromName = Brick::$builder->phrase->Get('sys', 'site_name');
+		$this->From = Brick::$builder->phrase->Get('sys', 'admin_mail'); 
+		$this->AltBody = "To view the message, please use an HTML compatible email viewer!";
+		$this->Priority = 3; 
+		$this->CharSet = "utf-8";
+	} 
+	
+	public function MsgHTML($message, $basedir=''){
+		$message = "<html><body>".$message."</body></html>";
+		parent::MsgHTML($message, $basedir);
+	}
+	
+	public function Send(){
+		if ($this->core->db->readonly){
+			return true;
+		}
+		return parent::Send();
+	}
+}
+
+
+
+?>
---
+++ module.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Модуль "Сообщения"
+ * 
+ * @version $Id$
+ * @package Abricos
+ * @subpackage Notify
+ * @copyright Copyright (C) 2008 Abricos All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * @author Alexander Kuzmin (roosit@abricos.org)
+ */
+
+$mod = new NotifyModule();
+CMSRegistry::$instance->modules->Register($mod);
+
+class NotifyModule extends CMSModule {
+	
+	private $_manager = null;
+	
+	function __construct(){
+		$this->version = "0.1";
+		$this->name = "notify";
+	}
+	
+	/**
+	 * Получить менеджер
+	 *
+	 * @return NotifyManager
+	 */
+	public function GetManager(){
+		if (is_null($this->_manager)){
+			require_once 'includes/phpmailer/class.phpmailer.php';
+			require_once 'includes/manager.php';
+			
+			$this->_manager = new NotifyManager($this);
+		}
+		return $this->_manager;
+	}
+}
+
+?>
