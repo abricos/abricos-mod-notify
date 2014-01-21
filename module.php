738ea089dad4228d700dc9ab66cd68c1140a9026
Alexander Kuzmin <roosit@abricos.org>
2014-01-21 09:38:40 +0400

    v0.1.2-dev

--- module.php
+++ module.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Notify Module
  * 
@@ -7,32 +8,31 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  * @author Alexander Kuzmin <roosit@abricos.org>
  */
-
 class NotifyModule extends Ab_Module {
-	
-	private $_manager = null;
-	
-	function __construct(){
-		$this->version = "0.1.1";
-		$this->name = "notify";
-	}
-	
-	/**
-	 * Получить менеджер
-	 *
-	 * @return NotifyManager
-	 */
-	public function GetManager(){
-		if (is_null($this->_manager)){
-			require_once 'includes/phpmailer/class.phpmailer.php';
-			require_once 'includes/manager.php';
-			
-			$this->_manager = new NotifyManager($this);
-		}
-		return $this->_manager;
-	}
+
+    private $_manager = null;
+
+    function __construct() {
+        $this->version = "0.1.2-dev";
+        $this->name = "notify";
+    }
+
+    /**
+     * Получить менеджер
+     *
+     * @return NotifyManager
+     */
+    public function GetManager() {
+        if (is_null($this->_manager)) {
+            require_once 'includes/phpmailer/class.phpmailer.php';
+            require_once 'includes/manager.php';
+
+            $this->_manager = new NotifyManager($this);
+        }
+        return $this->_manager;
+    }
+
 }
 
 Abricos::ModuleRegister(new NotifyModule());
-
 ?>
