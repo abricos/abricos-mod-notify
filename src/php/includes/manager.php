712bbc14cdcaa26577e9d9a9072107e3656db0db
Alexander Kuzmin <roosit@abricos.org>
2015-01-29 13:02:05 +0300

    remove save email to file functions

--- src/php/includes/manager.php
+++ src/php/includes/manager.php
@@ -14,64 +14,17 @@ class NotifyManager extends Ab_Notification {
      */
     public $module = null;
 
+    private static $_counter = 1;
+
     /**
      *
      * @var Ab_Database
      */
     public $db = null;
 
-    public $errorInfo = "";
-
-    private $emlcounter = 1;
-
     public function NotifyManager(NotifyModule $module){
         $this->module = $module;
         $this->db = Abricos::$db;
-    }
-
-    public function SendMail($email, $subject, $message, $from = '', $fromName = ''){
-        $cfg = isset(Abricos::$config['module']['notify']) ? Abricos::$config['module']['notify'] : array();
-
-        if (isset($cfg['totestfile']) && $cfg['totestfile']){
-
-            $filepath = CWD."/cache/eml";
-            @mkdir($filepath);
-            $filename = $filepath."/".date("YmdHis", time())."-".($this->emlcounter++).".htm";
-
-            $fh = @fopen($filename, 'a');
-
-            if (!$fh){
-                return false;
-            }
-
-            $str = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru" dir="ltr">
-<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
-<body class="yui-skin-sam">
-	<div style="width: 600px; margin: 0 auto;">';
-
-            $str .= "<p>E-mail: <b>".$email."</b><br />";
-            $str .= "From: ".$from."<br />";
-            $str .= "FromName: ".$fromName."<br />";
-            $str .= "Subject: <b>".$subject."</b><br />";
-            $str .= "Message (html code):</p>";
-            $str .= '<textarea style="width: 100%; height: 300px;">';
-            $str .= htmlspecialchars($message);
-            $str .= '</textarea>';
-
-            $str .= "<p>Message (preview):</p>";
-            $str .= "<div style='background-color: #F0F0F0;'>".$message."</div>";
-
-            $str .= '</div></body></html>';
-
-            fwrite($fh, $str);
-            fflush($fh);
-            fclose($fh);
-
-            return true;
-        } else {
-            return $this->SendByMailer($email, $subject, $message, $from, $fromName);
-        }
     }
 
     /**
@@ -101,8 +54,7 @@ class NotifyManager extends Ab_Notification {
      * @param string $from
      * @param string $fromName
      */
-
-    public function SendByMailer($email, $subject, $message, $from = '', $fromName = ''){
+    public function SendMail($email, $subject, $message, $from = '', $fromName = ''){
 
         $cfg = &Abricos::$config['module']['notify'];
 
@@ -146,6 +98,9 @@ class NotifyManager extends Ab_Notification {
                 $mailer->SMTPSecure = $cfg['SMTPSecure'];
             }
         }
+
+        $messageId = md5($message.(NotifyManager::$_counter++));
+        $this->messageId = $mailer->MessageID = $messageId;
 
         $mailer->Subject = $subject;
         $mailer->MsgHTML($message);
